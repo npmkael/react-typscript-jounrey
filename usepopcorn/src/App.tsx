b716c9c940a84e411f1f472cd35a79fb47fd4e0a
kieldoescode30 <kielycarreon123@gmail.com>
2024-09-01 08:51:58 +0800

    using useEffect hook for the first time

--- usepopcorn/src/App.tsx
+++ usepopcorn/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "./components/NavBar";
 import MovieBox from "./components/MovieBox";
 import { tempMovieDataType } from "./tempMovieModel";
@@ -8,204 +8,215 @@ import Search from "./components/Search";
 import NumResults from "./components/NumResults";
 
 const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
+    {
+        imdbID: "tt1375666",
+        Title: "Inception",
+        Year: "2010",
+        Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    },
+    {
+        imdbID: "tt0133093",
+        Title: "The Matrix",
+        Year: "1999",
+        Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+    },
+    {
+        imdbID: "tt6751668",
+        Title: "Parasite",
+        Year: "2019",
+        Poster: "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+    },
 ];
 
 const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
+    {
+        imdbID: "tt1375666",
+        Title: "Inception",
+        Year: "2010",
+        Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+        runtime: 148,
+        imdbRating: 8.8,
+        userRating: 10,
+    },
+    {
+        imdbID: "tt0088763",
+        Title: "Back to the Future",
+        Year: "1985",
+        Poster: "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+        runtime: 116,
+        imdbRating: 8.5,
+        userRating: 9,
+    },
 ];
 
-function App() {
-  const [movies, setMovies] = useState<tempMovieDataType[]>(tempMovieData);
-  const [watched, setWatched] =
-    useState<tempWatchedDataType[]>(tempWatchedData);
-  return (
-    <>
-      <NavBar>
-        <Logo />
-        <Search />
-        <NumResults movie={movies} />
-      </NavBar>
-      <MovieBox>
-        <Box>
-          <MovieList movieData={movies} />
-        </Box>
+const KEY = "fa23eaa3";
 
-        <Box>
-          <WatchSummary watchedData={watched} />
-          <WatchedList watchedData={watched} />
-        </Box>
-      </MovieBox>
-    </>
-  );
+function App() {
+    const [movies, setMovies] = useState<tempMovieDataType[]>([]);
+    const [watched, setWatched] = useState<tempWatchedDataType[]>([]);
+
+    // useEffect for data fetching
+    useEffect(function () {
+        fetch(
+            `https://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=interstellar`
+        )
+            .then((res) => res.json())
+            .then((data) => setMovies(data.Search));
+    }, []);
+
+    return (
+        <>
+            <NavBar>
+                <Logo />
+                <Search />
+                <NumResults movie={movies} />
+            </NavBar>
+            <MovieBox>
+                <Box>
+                    <MovieList movieData={movies} />
+                </Box>
+
+                <Box>
+                    <WatchSummary watchedData={watched} />
+                    <WatchedList watchedData={watched} />
+                </Box>
+            </MovieBox>
+        </>
+    );
 }
 
 function average(arr: number[]): number {
-  return arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+    return arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 }
 
 interface MovieDataProps {
-  movieData: tempMovieDataType[];
+    movieData: tempMovieDataType[];
 }
 
 interface Props {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 function Box({ children }: Props) {
-  const [isOpen, setIsOpen] = useState<boolean>(true);
+    const [isOpen, setIsOpen] = useState<boolean>(true);
 
-  return (
-    <div className="box">
-      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
-        {isOpen ? "–" : "+"}
-      </button>
-      {isOpen && children}
-    </div>
-  );
+    return (
+        <div className="box">
+            <button
+                className="btn-toggle"
+                onClick={() => setIsOpen((open) => !open)}
+            >
+                {isOpen ? "–" : "+"}
+            </button>
+            {isOpen && children}
+        </div>
+    );
 }
 
 function MovieList({ movieData }: MovieDataProps) {
-  return (
-    <ul className="list">
-      {movieData?.map((movie) => (
-        <Movie movie={movie} key={movie.imdbID} />
-      ))}
-    </ul>
-  );
+    return (
+        <ul className="list">
+            {movieData?.map((movie) => (
+                <Movie movie={movie} key={movie.imdbID} />
+            ))}
+        </ul>
+    );
 }
 
 type MovieProps = {
-  movie: tempMovieDataType;
+    movie: tempMovieDataType;
 };
 
 function Movie({ movie }: MovieProps) {
-  return (
-    <li>
-      <img src={movie.Poster} alt={`${movie.Title} poster`} />
-      <h3>{movie.Title}</h3>
-      <div>
-        <p>
-          <span>🗓</span>
-          <span>{movie.Year}</span>
-        </p>
-      </div>
-    </li>
-  );
+    return (
+        <li>
+            <img src={movie.Poster} alt={`${movie.Title} poster`} />
+            <h3>{movie.Title}</h3>
+            <div>
+                <p>
+                    <span>🗓</span>
+                    <span>{movie.Year}</span>
+                </p>
+            </div>
+        </li>
+    );
 }
 
 interface WatchedMovieProps {
-  watchedData: tempWatchedDataType[];
+    watchedData: tempWatchedDataType[];
 }
 
 function WatchSummary({ watchedData }: WatchedMovieProps) {
-  const avgImdbRating: number = average(
-    watchedData.map((movie) => movie.imdbRating)
-  );
-  const avgUserRating: number = average(
-    watchedData.map((movie) => movie.userRating)
-  );
-  const avgRuntime: number = average(watchedData.map((movie) => movie.runtime));
+    const avgImdbRating: number = average(
+        watchedData.map((movie) => movie.imdbRating)
+    );
+    const avgUserRating: number = average(
+        watchedData.map((movie) => movie.userRating)
+    );
+    const avgRuntime: number = average(
+        watchedData.map((movie) => movie.runtime)
+    );
 
-  return (
-    <div className="summary">
-      <h2>Movies you watched</h2>
-      <div>
-        <p>
-          <span>#️⃣</span>
-          <span>{watchedData.length} movies</span>
-        </p>
-        <p>
-          <span>⭐️</span>
-          <span>{avgImdbRating}</span>
-        </p>
-        <p>
-          <span>🌟</span>
-          <span>{avgUserRating}</span>
-        </p>
-        <p>
-          <span>⏳</span>
-          <span>{avgRuntime} min</span>
-        </p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="summary">
+            <h2>Movies you watched</h2>
+            <div>
+                <p>
+                    <span>#️⃣</span>
+                    <span>{watchedData.length} movies</span>
+                </p>
+                <p>
+                    <span>⭐️</span>
+                    <span>{avgImdbRating}</span>
+                </p>
+                <p>
+                    <span>🌟</span>
+                    <span>{avgUserRating}</span>
+                </p>
+                <p>
+                    <span>⏳</span>
+                    <span>{avgRuntime} min</span>
+                </p>
+            </div>
+        </div>
+    );
 }
 
 function WatchedList({ watchedData }: WatchedMovieProps) {
-  return (
-    <ul className="list">
-      {watchedData.map((movie) => (
-        <Watched movie={movie} key={movie.imdbID} />
-      ))}
-    </ul>
-  );
+    return (
+        <ul className="list">
+            {watchedData.map((movie) => (
+                <Watched movie={movie} key={movie.imdbID} />
+            ))}
+        </ul>
+    );
 }
 
 type WatchedMovie = {
-  movie: tempWatchedDataType;
+    movie: tempWatchedDataType;
 };
 
 function Watched({ movie }: WatchedMovie) {
-  return (
-    <li>
-      <img src={movie.Poster} alt={`${movie.Title} poster`} />
-      <h3>{movie.Title}</h3>
-      <div>
-        <p>
-          <span>⭐️</span>
-          <span>{movie.imdbRating}</span>
-        </p>
-        <p>
-          <span>🌟</span>
-          <span>{movie.userRating}</span>
-        </p>
-        <p>
-          <span>⏳</span>
-          <span>{movie.runtime} min</span>
-        </p>
-      </div>
-    </li>
-  );
+    return (
+        <li>
+            <img src={movie.Poster} alt={`${movie.Title} poster`} />
+            <h3>{movie.Title}</h3>
+            <div>
+                <p>
+                    <span>⭐️</span>
+                    <span>{movie.imdbRating}</span>
+                </p>
+                <p>
+                    <span>🌟</span>
+                    <span>{movie.userRating}</span>
+                </p>
+                <p>
+                    <span>⏳</span>
+                    <span>{movie.runtime} min</span>
+                </p>
+            </div>
+        </li>
+    );
 }
 
 export default App;
--- usepopcorn/src/main.tsx
+++ usepopcorn/src/main.tsx
@@ -5,27 +5,26 @@ import { createRoot } from "react-dom/client";
 import { useState } from "react";
 
 import StarRating from "./components/StarRating.tsx";
+import App from "./App.tsx";
+import "./index.css";
 
 function Test() {
-  const [movieRating, setMovieRating] = useState<number>(0);
+    const [movieRating, setMovieRating] = useState<number>(0);
 
-  return (
-    <div>
-      <StarRating color="blue " maxRating={10} onSetRating={setMovieRating} />
-      <p>This movies was rated {movieRating} Stars</p>
-    </div>
-  );
+    return (
+        <div>
+            <StarRating
+                color="blue "
+                maxRating={10}
+                onSetRating={setMovieRating}
+            />
+            <p>This movies was rated {movieRating} Stars</p>
+        </div>
+    );
 }
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating maxRating={5} size={24} color="red" defaultRating={3} />
-
-    <Test />
-  </StrictMode>
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
