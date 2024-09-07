38f5c376ba760daa3869d776b0fe7462f209f23a
kieldoescode30 <kielycarreon123@gmail.com>
2024-09-07 23:52:48 +0800

    added more functionalities

--- todo-list-v2/src/App.tsx
+++ todo-list-v2/src/App.tsx
@@ -1,103 +1,217 @@
 import { useState } from "react";
-import { FaPencilAlt } from "react-icons/fa";
+import { BsPlus } from "react-icons/bs";
 
 const initialTodos = [
-  {
-    title: "play apex",
-    description:
-      "will play with friends later at some point cause I have busy friends...",
-  },
-  {
-    title: "coding session",
-    description: "will code later since i'm an asshole...",
-  },
-  {
-    title: "watch the frog",
-    description: "go minsi is so hot...",
-  },
+    {
+        id: "1",
+        title: "play apex",
+        description:
+            "will play with friends later at some point cause I have busy friends...",
+        isDone: false,
+    },
+    {
+        id: "2",
+        title: "coding session",
+        description: "will code later since i'm an asshole...",
+        isDone: false,
+    },
+    {
+        id: "3",
+        title: "watch the frog",
+        description: "go minsi is so hot...",
+        isDone: false,
+    },
 ];
 
 type TodoType = {
-  title: string;
-  description: string;
+    title: string;
+    description: string;
+    isDone: boolean;
+    id: string;
 };
 
 function App() {
-  const [todo, setTodos] = useState<TodoType[]>(initialTodos);
-  const [active, setIsAcive] = useState<number>(0);
+    const [todo, setTodos] = useState<TodoType[]>(initialTodos);
+    const [active, setIsAcive] = useState<number>(0);
+    const [toggleTask, setToggleTask] = useState<boolean>(false);
 
-  return (
-    <>
-      <div className="todo-container">
-        <div className="navbar">
-          <Button
-            className={active === 0 ? "home btn active" : "home btn"}
-            onClick={() => setIsAcive(0)}
-          >
-            Home
-          </Button>
-          <Button
-            className={active === 1 ? "todo btn active" : "todo btn"}
-            onClick={() => setIsAcive(1)}
-          >
-            Todo
-          </Button>
-          <span className="todo-counter">{todo.length}</span>
-        </div>
-        <div className={active === 1 ? "line left" : "line"}></div>
-      </div>
-      <div className="main-container">
-        {active === 1 ? (
-          <>
-            {todo.map((todo) => (
-              <Todo title={todo.title} description={todo.description} />
-            ))}
-            <div className="add-btn-container">
-              <Button className="add-todo-btn">
-                <FaPencilAlt color="white" />
-              </Button>
+    function handleAddTodo(newTodo: TodoType): void {
+        setTodos((todo) => [...todo, newTodo]);
+    }
+
+    return (
+        <>
+            <div className="todo-container">
+                {/* extract these to components */}
+                <div className="navbar">
+                    <Button
+                        className={
+                            active === 0 ? "home btn active" : "home btn"
+                        }
+                        onClick={() => {
+                            setIsAcive(0);
+                            setToggleTask(false);
+                        }}
+                    >
+                        Home
+                    </Button>
+                    <Button
+                        className={
+                            active === 1 ? "todo btn active" : "todo btn"
+                        }
+                        onClick={() => {
+                            setIsAcive(1);
+                            setToggleTask(false);
+                        }}
+                    >
+                        Todo
+                    </Button>
+                    <span className="todo-counter">{todo.length}</span>
+                </div>
+                <div className="main-container">
+                    {active === 1 ? (
+                        <>
+                            {todo.map((todo) => (
+                                <Todo todo={todo} key={todo.id} />
+                            ))}
+                            <div className="add-btn-container">
+                                <Button
+                                    className="add-todo-btn"
+                                    onClick={() =>
+                                        setToggleTask((toggle) => !toggle)
+                                    }
+                                >
+                                    <BsPlus color="white" size={30} />
+                                    New Task
+                                </Button>
+                            </div>
+                            <CreateTaskModal
+                                toggleTask={toggleTask}
+                                onHandleTodo={handleAddTodo}
+                                onSetToggle={setToggleTask}
+                            />
+                        </>
+                    ) : (
+                        <p>home</p>
+                    )}
+                </div>
             </div>
-          </>
-        ) : (
-          <p>home</p>
-        )}
-      </div>
-      <div className="sample-container"></div>
-    </>
-  );
+
+            <div className="main-todo-wrapper">
+                <div>
+                    <p>title</p>
+                </div>
+            </div>
+        </>
+    );
 }
 
 type ButtonProps = {
-  children: React.ReactNode;
-  className: string;
-  onClick?: () => void;
+    children: React.ReactNode;
+    className: string;
+    onClick?: () => void;
 };
 
 function Button({ className, children, onClick }: ButtonProps) {
-  return (
-    <button
-      className={className}
-      onClick={() => {
-        if (onClick) onClick();
-      }}
-    >
-      {children}
-    </button>
-  );
+    return (
+        <button
+            className={className}
+            onClick={() => {
+                if (onClick) onClick();
+            }}
+        >
+            {children}
+        </button>
+    );
 }
 
-function Todo({ title, description }: TodoType) {
-  return (
-    <>
-      <div className="todo-wrapper">
-        <input type="checkbox" />
-        <div className="todo-info">
-          <p className="title">{title}</p>
-          <p className="description">{description}</p>
-        </div>
-      </div>
-    </>
-  );
+type TodoProps = {
+    todo: TodoType;
+};
+
+function Todo({ todo }: TodoProps) {
+    return (
+        <>
+            <div className="todo-wrapper">
+                <input type="checkbox" />
+                <div className="todo-info">
+                    <p className="title">{todo.title}</p>
+                    <p className="description">{todo.description}</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+type CreateTaskModalProps = {
+    toggleTask: boolean;
+    onHandleTodo: (newTodo: TodoType) => void;
+    onSetToggle: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+function CreateTaskModal({
+    toggleTask,
+    onHandleTodo,
+    onSetToggle,
+}: CreateTaskModalProps) {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    function addTodo(e: React.FormEvent) {
+        e.preventDefault();
+
+        if (!title && !description) return;
+
+        const newTodo = {
+            id: String(Date.now()),
+            title,
+            description,
+            isDone: false,
+        };
+
+        onHandleTodo(newTodo);
+        setTitle("");
+        setDescription("");
+        onSetToggle(false);
+    }
+
+    return (
+        <form
+            className={
+                toggleTask ? "sample-container active" : "sample-container"
+            }
+            onSubmit={(e) => addTodo(e)}
+        >
+            <div>
+                <input
+                    type="text"
+                    placeholder="Task..."
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+            </div>
+            <div>
+                <textarea
+                    name=""
+                    id=""
+                    placeholder="Description..."
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
+            </div>
+            <div className="button-container">
+                <button
+                    className="btn cancel"
+                    onClick={() => onSetToggle(false)}
+                >
+                    Cancel
+                </button>
+                <button className="btn add" type="submit">
+                    Add
+                </button>
+            </div>
+        </form>
+    );
 }
 
 export default App;
