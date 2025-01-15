a0026f240f4b82b754b3acdfb90cd280b6a376cd
kieldoescode30 <kielycarreon123@gmail.com>
2025-01-15 15:46:28 +0800

    244-245: Profiler Dev Tool & Optimization trick with children

--- atomic-blog-tsx-ver/src/components/Post/Posts.tsx
+++ atomic-blog-tsx-ver/src/components/Post/Posts.tsx
@@ -1,4 +1,5 @@
 import { usePosts } from "../../context/PostProvider";
+import Test from "../../Test";
 
 const Posts = () => {
   return (
@@ -14,14 +15,17 @@ const List = () => {
   const { posts } = context;
 
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+      {/* <Test /> */}
+    </>
   );
 };
 
