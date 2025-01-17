92c6047eddfb5063985e878bf3e94d9343ffdc2a
kieldoescode30 <kielycarreon123@gmail.com>
2025-01-17 17:24:26 +0800

    249: useMemo in Practice

--- atomic-blog-tsx-ver/src/App-memo.tsx
+++ atomic-blog-tsx-ver/src/App-memo.tsx
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from "react";
+import { memo, useEffect, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 import { Post } from "./types";
 
@@ -47,10 +47,12 @@ function App() {
     [isFakeDark]
   );
 
-  const archiveOptions = {
-    show: false,
-    title: "Post archive in addition to main posts",
-  };
+  const archiveOptions = useMemo(() => {
+    return {
+      show: false,
+      title: `Post archive in addition to ${posts.length} main posts`,
+    };
+  }, [posts.length]);
 
   return (
     <section>
